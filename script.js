a125d9b8c29554ac28ec154726a4bc063eae1e84
paulhassan151-ai <paulhassan151@gmail.com>
2026-08-05 15:15:55 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function searchCars() {
+let input=document.getElementById("search").value.toLowerCase();
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+let text=card.innerText.toLowerCase();
+card.style.display=text.includes(input)?"block":"none";
+});
+}
